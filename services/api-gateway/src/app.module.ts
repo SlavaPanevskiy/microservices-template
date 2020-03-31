ad966b1f530f852a2bf8ffefdf496ce0c1c70a43
slava.p <slava.p@techery.io>
2020-03-31 15:24:40 +0300

    Remove the root controllers from ApiGateway

--- services/api-gateway/src/app.module.ts
+++ services/api-gateway/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatalogModule } from './catalog/catalog.module';
 
 // Only generate the TS interfaces when not running in production
@@ -22,7 +20,7 @@ const gqlModule =
 
 @Module({
   imports: [gqlModule, CatalogModule],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
